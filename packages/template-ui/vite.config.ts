58fa3e1f2405728a198525d06f8407ae38ba19de
liangjunqi <247948681@qq.com>
2024-02-27 15:41:30 +0800

    build configs

--- packages/template-ui/vite.config.ts
+++ packages/template-ui/vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),dts()],
+  build: {
+    lib: {
+      name: 'radix-vue',
+      fileName: 'index',
+      entry: resolve(__dirname, 'src/index.ts')
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          Vue: 'vue'
+        }
+      }
+    }
+  }
 })
